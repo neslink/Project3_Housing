53ea342111cfe88e10226c638a437b1c04086368
Chris Baker <cbake105@uncc.edu>
2023-10-29 16:55:13 -0400

    cleaned up page

---
+++ data/InflationRate.js
@@ -0,0 +1,46 @@
+﻿let InflationRate = [
+    {
+        "Year": "2013",
+        "Inflation_Rate": "1.5"
+    },
+    {
+        "Year": "2014",
+        "Inflation_Rate": "1.6"
+    },
+    {
+        "Year": "2015",
+        "Inflation_Rate": "0.1"
+    },
+    {
+        "Year": "2016",
+        "Inflation_Rate": "1.3"
+    },
+    {
+        "Year": "2017",
+        "Inflation_Rate": "2.1"
+    },
+    {
+        "Year": "2018",
+        "Inflation_Rate": "2.4"
+    },
+    {
+        "Year": "2019",
+        "Inflation_Rate": "1.8"
+    },
+    {
+        "Year": "2020",
+        "Inflation_Rate": "1.3"
+    },
+    {
+        "Year": "2021",
+        "Inflation_Rate": "4.7"
+    },
+    {
+        "Year": "2022",
+        "Inflation_Rate": "8"
+    },
+    {
+        "Year": "2023",
+        "Inflation_Rate": "4.1"
+    }
+]
